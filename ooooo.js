b85f02a4fdac49b1192cc8b299e1299f8d5cc403
rahoma615 <81978766+rahoma615@users.noreply.github.com>
2022-03-28 14:24:00 +0200

    Add files via upload

---
+++ ooooo.js
@@ -0,0 +1,24 @@
+
+
+
+function cToF(celsius) 
+{
+  var cTemp = celsius;
+  var cToFahr = cTemp * 9 / 5 + 32;
+  var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+    console.log(message);
+
+ var span = document.createElement('span');
+  span.innerHTML = s;
+  return span.textContent || span.innerText;
+}
+
+function fToC(fahrenheit) 
+{
+  var fTemp = fahrenheit;
+  var fToCel = (fTemp - 32) * 5 / 9;
+  var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+    console.log(message);
+} 
+cToF(60);
+fToC(45);
